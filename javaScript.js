c332625ee749c0ca616e0efaf34bdd70effd3c6b
Lawal Akande <lawalsaliouakande@gmail.com>
2020-07-07 23:59:28 -0700

    adding my first commit

---
+++ javaScript.js
@@ -0,0 +1,41 @@
+// Academics
+function showAcademics(){
+    document.getElementById('overAcademics').style.zIndex = '1';
+    document.getElementById('overAcademics').style.display = 'block';
+    
+}
+function clearAcademics(){
+    document.getElementById('overAcademics').style.display = 'none';
+}
+
+// About Us
+function showAboutUs(){
+    document.getElementById('overAboutUs').style.display = 'block';
+}
+function clearAboutUs(){
+    document.getElementById('overAboutUs').style.display = 'none';
+}
+
+// Units
+function showUnits(){
+    document.getElementById('overUnits').style.display = 'block';   
+}
+function clearUnits(){
+    document.getElementById('overUnits').style.display = 'none';
+}
+
+// Resources
+function showResources(){
+    document.getElementById('overResources').style.display = 'block';   
+}
+function clearResources(){
+    document.getElementById('overResources').style.display = 'none';
+}
+
+// Admission
+function showAdmissions(){
+    document.getElementById('overAdmissions').style.display = 'block';
+}
+function clearAdmissions(){
+    document.getElementById('overAdmissions').style.display = 'none';
+}
